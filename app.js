91228b190f8cf97f9219f5cdd0925c446331f1eb
vtloupas <vtloupas@knowledge.gr>
2020-09-09 17:37:29 +0300

    heroku changes

--- app.js
+++ app.js
@@ -4,11 +4,21 @@ const cors = require('cors');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const PORT = process.env.PORT || 5000;
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+//app.use(express.static(path.join(__dirname, "client/build")));
+
+if(process.env.NODE_ENV === "production"){
+  app.use(express.static(path.join(__dirname, "client/build")));
+}
+
+console.log(__dirname);
+console.log(path.join(__dirname, "client/build"));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -40,6 +50,10 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is staring on port ${PORT}`)
 });
 
 module.exports = app;
